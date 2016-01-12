09b25ea2899f5bfc9fea9854b9b61346630d69f4
Philip Sarin <psarin@managedbyq.com>
2016-01-11 22:22:01 -0500

    indentation and slack integration

--- tiberius.js
+++ tiberius.js
@@ -1,53 +1,71 @@
 var async = require('async');
 var git = require('simple-git');
 var NodeGit = require('NodeGit');
+var Slack = require('slack-node');
 var _ = require('lodash');
 
 function fullCommitMessage(hash, cb) {
-    NodeGit.Repository.open(process.cwd())
-        .then(function(repo) {
-            return repo.getCommit(hash);
-        })
-        .then(function(commit) {
-            cb(null, commit.message());
-        })
-        .catch(function(err) {
-            cb(err);
-        });
+  NodeGit.Repository.open(process.cwd())
+    .then(function (repo) {
+      return repo.getCommit(hash);
+    })
+    .then(function (commit) {
+      cb(null, commit.message());
+    })
+    .catch(function (err) {
+      cb(err);
+    });
 }
 
-module.exports.releaseNotes = function(repoName, firstTag, secondTag, cb) {
-    var linkTemplate = _.template('https://github.com/managedbyq/<%= repoName %>/pull/<%= num %>/files');
-    var lineTemplate = _.template('\u2022 (<%= author %>) <%= description %> - <<%= link %>|github>');
-    git(process.cwd()).log({from: firstTag, to: secondTag}, function (err, log) {
-        if (err) {
-            cb(err);
-        }
+module.exports.sendSlackMessage = function (options, cb) {
+  options = _.extend({channel: '#dev-ci'}, options || {});
+  slack = new Slack(options.apiToken);
+  slack.api('chat.postMessage', {
+    channel: options.channel,
+    attachments: JSON.stringify([{
+      'text': options.message,
+      'color': 'warning',
+      'mrkdwn_in': ["text"]
+    }]),
+    icon_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Tiberius%2C_Romisch-' +
+              'Germanisches_Museum%2C_Cologne_%288115606671%29.jpg/440px-Tiberius' +
+              '%2C_Romisch-Germanisches_Museum%2C_Cologne_%288115606671%29.jpg',
+    username: 'Tiberius'
+  }, cb);
+};
 
-        var prCommits = _.filter(log.all, function(commit) {
-            return commit.message.startsWith("Merge pull request");
-        });
+module.exports.releaseNotes = function (repoName, firstTag, secondTag, cb) {
+  var linkTemplate = _.template('https://github.com/managedbyq/<%= repoName %>/pull/<%= num %>/files');
+  var lineTemplate = _.template('\u2022 (<%= author %>) <%= description %> - <<%= link %>|github>');
+  git(process.cwd()).log({from: firstTag, to: secondTag}, function (err, log) {
+    if (err) {
+      cb(err);
+    }
 
-        async.parallel(_.map(prCommits, function(commit) {
-            return function(cb) {
-                fullCommitMessage(commit.hash.substr(1), function (err, message) {
-                    if (err) {
-                        cb(err);
-                    }
-                    var prNumber = message.match(/Merge pull request #(\d+)/)[1];
-                    var description = message.split('\n', 3)[2];
-
-                    var link = linkTemplate({num: prNumber, repoName: repoName});
-                    var line = lineTemplate({author: commit.author_name, description: description, link: link});
-                    cb(null, line);
-                });
-            }
-        }), function(err, results) {
-            if (err) {
-                cb(err);
-            }
-            results.reverse();
-            cb(null, results.join('\n'));
-        });
+    var prCommits = _.filter(log.all, function (commit) {
+      return commit.message.startsWith("Merge pull request");
     });
+
+    async.parallel(_.map(prCommits, function (commit) {
+      return function (cb) {
+        fullCommitMessage(commit.hash.substr(1), function (err, message) {
+          if (err) {
+            cb(err);
+          }
+          var prNumber = message.match(/Merge pull request #(\d+)/)[1];
+          var description = message.split('\n', 3)[2];
+
+          var link = linkTemplate({num: prNumber, repoName: repoName});
+          var line = lineTemplate({author: commit.author_name, description: description, link: link});
+          cb(null, line);
+        });
+      }
+    }), function (err, results) {
+      if (err) {
+        cb(err);
+      }
+      results.reverse();
+      cb(null, results.join('\n'));
+    });
+  });
 };
