ab2e41a40e76eaa75160691b28834a169a7bc647
Philip Sarin <psarin@managedbyq.com>
2016-01-19 08:24:58 -0500

    move version bumping to gulp-utils

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
 'use strict';
 
 var browserify = require('browserify');
 var buffer = require('vinyl-buffer');
+var exec = require('child_process').exec;
+var fs = require('fs');
 var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var livereload = require('gulp-livereload');
@@ -19,13 +20,13 @@ function handleError(err) {
   }
 }
 
-module.exports.browserifyHelper = function(inputFile, options) {
+module.exports.browserifyHelper = function (inputFile, options) {
   var bundle = browserify(inputFile, options)
-    .on('log', function(msg) {
+    .on('log', function (msg) {
       console.log(options.moduleName.green, msg);
     });
 
-  var rebundle = function(){
+  var rebundle = function () {
     return bundle.bundle()
       .on('error', handleError)
       .pipe(source(inputFile))
@@ -42,6 +43,59 @@ module.exports.browserifyHelper = function(inputFile, options) {
   }
 
   return rebundle();
+};
+
+var BUMP_BRANCH = 'automation/bump';
+var MAIN_BRANCH = 'master';
+
+module.exports.bumpVersion = function (packageConfigPath, cb) {
+
+  if (isSameCommitHash('origin/' + MAIN_BRANCH, 'origin/' + BUMP_BRANCH)) {
+    console.log('Bump commit already created, aborting.');
+    return cb();
+  }
+
+  var packageConfig = require(packageConfigPath);
+  var versionArray = packageConfig.version.split('.');
+  // increment patch version
+  versionArray[2] = parseInt(versionArray[2], 10) + 1;
+  packageConfig.version = versionArray.join('.');
+  fs.writeFile(packageConfigPath, JSON.stringify(packageConfig, null, 2), function (err) {
+    if (err) {
+      console.log('Error writing to \'package.json\'\n'.bold.red);
+      return cb(err);
+    }
+
+    var commitCommand = 'git add package.json && git commit -m "[bump] ' + packageConfig.version + '"';
+
+    exec(commitCommand, function (err) {
+      if (err) {
+        console.log('Error commiting changes to \'package.json\'\n'.bold.red);
+        return cb(err);
+      }
+
+      var commands = [
+        'git branch -f ' + MAIN_BRANCH + ' HEAD',   //Set main branch head to detached head
+        'git branch -f ' + BUMP_BRANCH + ' HEAD',   //Set bump branch head to detached head
+        'git push origin ' + MAIN_BRANCH,           //Will fail if origin branch has changes (intended)
+        'git push -f origin ' + BUMP_BRANCH,        //This can be forced if preceding succeeds
+      ];
+      exec(commands.join(' && '), function (err, stdout) {
+        if (err) {
+          var out = String(stdout);
+          console.log(out);
+
+          var log = String(execSync('git log -3'));
+          console.log(log);
+
+          console.log('Error pushing bump commits'.bold.red);
+          return cb(err);
+        }
+
+        cb();
+      });
+    });
+  });
 };
 
 module.exports.deployer = require('./lib/deployer');
